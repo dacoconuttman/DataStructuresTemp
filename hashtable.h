cbb5f389afa86988d6acfff535c8f34bcc16d9b7
laxmatt08 <laxmatt08@hotmail.com>
2015-11-16 22:01:18 -0600

    Create hashtable.h

---
+++ hashtable.h
@@ -0,0 +1,15 @@
+#include "hashtable.h"
+
+hashtable::hashtable()
+{
+
+    //initialize an arry of 26 elements
+    //impliment element on to an AVL tree
+
+}
+
+
+hashtable::getTotalPuts()
+{
+    return wordCounter;
+}
