9332fcf01bbe3e05d71bc8fd96c3c79fb7dfd10d
Lorsen <omarfroa@gmail.com>
2015-11-18 08:44:26 -0600

    Avl start

---
+++ indexinterface.h
@@ -0,0 +1,23 @@
+#ifndef INDEXINTERFACE
+#define INDEXINTERFACE
+#include <iostream>
+#include <vector>
+#include <word.h>
+
+
+class IndexInterface
+{
+   public:
+      // pure virtual function
+    virtual std::vector<int> get(std::string) =0;
+    virtual void put(word) =0;
+    virtual void dumptoFile() =0;
+    virtual void pullfromFile() =0;
+    virtual void deleteAll() =0;
+    virtual int getTotalPuts() =0;
+
+   private:
+
+};
+
+      #endif // INDEXINTERFACE
