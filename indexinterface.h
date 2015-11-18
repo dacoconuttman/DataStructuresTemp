3786c95e241cad22e58b31c9c63bdf767570cca5
laxmatt08 <laxmatt08@hotmail.com>
2015-11-18 16:10:50 -0600

    Update indexinterface.h

--- indexinterface.h
+++ indexinterface.h
@@ -10,7 +10,7 @@ class IndexInterface
    public:
       // pure virtual function
     virtual std::vector<int> get(std::string) =0;
-    virtual void put(word) =0;
+    virtual void put(Word*) =0;
     virtual void dumptoFile() =0;
     virtual void pullfromFile() =0;
     virtual void deleteAll() =0;
