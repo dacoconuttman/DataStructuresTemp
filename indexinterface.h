e818d2b47aede6a5e0279d3a129180953cd4861e
laxmatt08 <laxmatt08@hotmail.com>
2015-11-17 20:44:19 -0600

    Update indexinterface.h

--- indexinterface.h
+++ indexinterface.h
@@ -2,6 +2,7 @@
 #define INDEXINTERFACE
 #include <iostream>
 #include <vector>
+#include <word.h>
 
 
 class IndexInterface
@@ -9,7 +10,7 @@ class IndexInterface
    public:
       // pure virtual function
     virtual std::vector<int> get(std::string) =0;
-    virtual void put(std::string, int) =0;
+    virtual void put(word) =0;
     virtual void dumptoFile() =0;
     virtual void pullfromFile() =0;
     virtual void deleteAll() =0;
