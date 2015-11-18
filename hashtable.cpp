2a902498152348b830a1084950e2af8aa6a4f31f
laxmatt08 <laxmatt08@hotmail.com>
2015-11-18 16:08:32 -0600

    Update hashtable.cpp

--- hashtable.cpp
+++ hashtable.cpp
@@ -6,33 +6,82 @@ using namespace std;
 hashtable::hashtable()
 {
 
-    //initialize an arry of 26 elements
-    //impliment element on to an AVL tree
-
 
 }
 
 //Pull some shit outta this
-vector<int> hashtable::get(string)
+vector<int> hashtable::get(string toSearch)
 {
+    int pos = toSearch[0];
+    pos = pos % 25;
+
+
+    if(hashIndex[pos] == NULL)
+    {
+        std::vector<int> emptyVec;
+        return emptyVec;
+    }
+
+    LinkedListNode* temp = hashIndex[pos];
+    while(temp->next != NULL && temp->data->word !=toSearch)
+    {
+        temp = temp->next;
+    }
+    if(temp->data->word == toSearch)
+    {
+        return temp->data->articles;
+    }
+    else
+    {
+        std::vector<int> emptyVec;
+        return emptyVec;
+    }
 
 }
 
 //Throw this shit in here
-void hashtable::put(word)
+void hashtable::put(Word* toInsert)
 {
+    wordCounter++;
+
+    int pos = toInsert->word[0];
+    pos = pos % 25; //Account for ascii and find the right bucket for the goods
+
+    //First node detection
+    if(hashIndex[pos] == NULL)
+    {
+        hashIndex[pos] = new LinkedListNode;
+        hashIndex[pos]->data = toInsert;
+        return;
+    }
+
+    LinkedListNode* temp = hashIndex[pos];
+    while(temp->next != NULL && temp->data->word != toInsert->word)
+    {
+        temp = temp->next;
+    }
+    if(temp->data->word == toInsert->word)
+    {
+        //Words are same, need to add page num to vector
+        temp->data->articles.push_back(toInsert->articles[0]);
+        delete toInsert;
+        return;
+    }
+    temp->next = new LinkedListNode;
+    temp->next->data = toInsert;
+
+
 
 }
 
 // De-allocates all memory used for the Hash Table.
 hashtable::~hashtable()
 {
-    //Delete each AVL tree first
-    delete [] array;
+
 }
 
 //a counter to see how many words are in the total ammount
-hashtable::getTotalPuts()
+int hashtable::getTotalPuts()
 {
     return wordCounter;
 }
